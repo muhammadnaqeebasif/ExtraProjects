34c21df9313b31644e7b64174baa758861178854
naqeebasif <naqeebasif@gmail.com>
2018-07-01 20:57:03 +0100

    FacebookFriends

--- FacebookFriends/src/com/hadoop/FacebookFriendsReducer.java
+++ FacebookFriends/src/com/hadoop/FacebookFriendsReducer.java
@@ -7,31 +7,39 @@ import java.util.List;
 
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class FacebookFriendsReducer extends Reducer<FriendPair, FriendArray, FriendPair, FriendArray> {
+public class FacebookFriendsReducer extends
+		Reducer<FriendPair, FriendArray, FriendPair, FriendArray> {
 	@Override
-	protected void reduce(FriendPair key, Iterable<FriendArray> values,
+	protected void reduce(
+			FriendPair key,
+			Iterable<FriendArray> values,
 			Reducer<FriendPair, FriendArray, FriendPair, FriendArray>.Context context)
 			throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
-		List<Friend[]> list=new ArrayList<Friend[]>();
-		List<Friend> commonFriend=new ArrayList<Friend>();
-		int count=0;
-		for(FriendArray value:values){
-			Friend []toadd=Arrays.copyOf(value.get(), value.get().length, Friend[].class);
+		List<Friend[]> list = new ArrayList<Friend[]>();
+		List<Friend> commonFriend = new ArrayList<Friend>();
+		int count = 0;
+		for (FriendArray value : values) {
+			Friend[] toadd = Arrays.copyOf(value.get(), value.get().length,
+					Friend[].class);
 			list.add(toadd);
 			count++;
 		}
-		if(count!=2){
+		if (count != 2) {
 			return;
 		}
-		for(Friend outerF:list.get(0)){
-			for(Friend innerF:list.get(1)){
-				if(outerF.equals(innerF)){
-					commonFriend.add(innerF);
+		for (Friend outerF : list.get(0)) {
+			for (Friend innerF : list.get(1)) {
+				if (outerF.equals(innerF)) {
+					if (!(key.getFirst().equals(innerF) || key.getSecond()
+							.equals(innerF))) {
+						commonFriend.add(innerF);
+					}
 				}
 			}
 		}
-		Friend cfArray[]=Arrays.copyOf(commonFriend.toArray(), commonFriend.toArray().length, Friend[].class);
+		Friend cfArray[] = Arrays.copyOf(commonFriend.toArray(),
+				commonFriend.toArray().length, Friend[].class);
 		context.write(key, new FriendArray(Friend.class, cfArray));
 	}
 
